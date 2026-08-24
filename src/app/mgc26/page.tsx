026cce646bdb564cb94aa57a6db7749feb76c5fe
creakin <veganstreampie@proton.me>
2026-08-24 13:00:52 +0200

    MGC26 promo

---
+++ src/app/mgc26/page.tsx
@@ -0,0 +1,83 @@
+import { IndexLink } from "@/components/index-link";
+import type { Metadata } from "next";
+import Image from "next/image";
+import { Suspense } from "react";
+import Mgc26Logo from "@/assets/mgc26.jpg";
+
+const metadata: Metadata = {
+  title: "Materialist Game Con 2026 - Spel, politik, gemenskap",
+  description:
+    "MGL presenterar: Materialist Game Con 2026, 9-11 oktober i Malmö",
+  openGraph: {
+    images: [
+      {
+        url: `https://mglabs.se/${Mgc26Logo.src}`,
+        width: 630,
+        height: 630,
+        alt: "Materialist Game Con 2026",
+      },
+    ],
+  },
+};
+
+export { metadata };
+
+export default async function Page() {
+  return (
+    <Suspense fallback={"Laddar..."}>
+      <IndexLink />
+      <article className="col-span-5 grid-cols-5 gap-8 md:grid">
+        <div className="col-span-2 mb-8 text-center">
+          <Image
+            src={Mgc26Logo.src}
+            width={600}
+            height={300}
+            alt="Materialist Game Labs logo"
+          />
+          <h2 className="text-4xl font-bold">9 - 11 oktober</h2>
+          <h3 className="font-bold">
+            Kvarnby Folkhögskola - Scheelegatan 7, Malmö{" "}
+          </h3>
+        </div>
+
+        <div className="col-span-3">
+          <p className="text-lg text-muted-foreground">MGL presenterar:</p>
+          <h1 className="mb-4 text-2xl font-bold md:text-4xl">
+            Materialist Game Con 2026
+          </h1>
+          <div className="text-xl md:text-2xl">
+            <p className="mb-4">
+              Detta är ett konvent för människor som vill utforska spel ur ett
+              socialistiskt DIY-perspektiv. Vi söker spelkonstruktörer,
+              spelledare, figurspelare, brädspelsnördar, rollspelare,
+              aktivister, tänkare, konstnärer, hackers, gruntar och nyfikna
+              kamrater.
+            </p>
+
+            <p className="mb-4">
+              Det kan fortfarande finnas plats i programmet! Hör av dig om du
+              vill hjälpa till eller arrangera något:
+            </p>
+
+            <p className="mb-4">
+              <a
+                href="mailto:materialistgamelabs@protonmail.com"
+                target="_blank"
+                className="underline"
+              >
+                materialistgamelabs@proton.me
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <hr className="col-span-5"/>
+
+        <div className="col-span-5 col-start-2 italic">
+          Mer info och programpunkter kommer snart...
+          
+        </div>
+      </article>
+    </Suspense>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ import { MaterialsList } from "@/components/materials-list";
 import TagList from "@/components/tag-list";
 
 import Logo from "@/assets/logo.jpg"
+import Mgc26Logo from "@/assets/mgc26.jpg"
 import { Sorting } from "@/components/sorting";
+import Link from "next/link";
 
 export const dynamic = "force-static";
 export const revalidate = false;
@@ -41,10 +43,30 @@ export default async function Home() {
         <h1 className="mb-1 font-medium">
           Materialist Game Labs arkiv
         </h1>
-        <p className="mb-12 text-muted-foreground">
+        <p className="mb-8 text-muted-foreground">
           Välkommen till mglabs.se! Här finner du material skapat av medlemmar i skaparkollektivet
           Materialist Game Labs.
         </p>
+
+        <Link href="/mgc26" className="mb-12 block text-center sm:text-left sm:grid grid-cols-10 gap-4 p-4 items-center border-2 border-[#CC1312]/50 hover:border-[#CC1312] transition-colors">
+          <Image
+            src={Mgc26Logo.src}
+            width={300}
+            height={300}
+            alt="Materialist Game Labs logo"
+            className="col-span-3 mx-auto"
+          />
+          <div className="col-span-7 font-bold">
+            <p className="text-muted-foreground">MGL presenterar:</p>
+            <p className="text-2xl">
+              Materialist Game Con 2026<br />
+            </p>
+            <p className="">9 - 11 oktober, Malmö</p>
+            <p className="">Läs mer här</p>
+
+          </div>
+
+        </Link>
 
         <div className="border-b border-muted-foreground/50 pb-4 mb-4">
           <h2 className="text-sm font-medium mb-2">Filtrera på taggar</h2>
