f7f032f3f4643f54982d68e814754d10b31d765f
creakin <veganstreampie@proton.me>
2026-09-23 10:56:15 +0200

    mgc content

--- src/app/mgc26/page.tsx
+++ src/app/mgc26/page.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import { Suspense } from "react";
 import Mgc26Logo from "@/assets/mgc26.jpg";
+import Johanna from "@/assets/johanna.jpg";
+import Oceania from "@/assets/oceania.jpg";
 
 const metadata: Metadata = {
   title: "Materialist Game Con 2026 - Spel, politik, gemenskap",
@@ -71,12 +73,103 @@ export default async function Page() {
           </div>
         </div>
 
-        <hr className="col-span-5"/>
+        <hr className="col-span-5" />
 
-        <div className="col-span-5 col-start-2 italic">
-          Mer info och programpunkter kommer snart...
-          
+        <div className="italic text-center col-span-5 my-4">Programmet fylls på löpande...</div>
+    
+        <div className="col-span-5 md:grid grid-cols-6 gap-8">
+          <h2 className="col-span-6 text-2xl font-bold underline text-center uppercase my-8">Lördag</h2>
+          <div className="col-span-3">
+            <div className="relative">
+              <Image
+                src={Johanna.src}
+                width={600}
+                height={600}
+                alt="Johanna Koljonen"
+                className="aspect-square flex-grow-0 object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 border-l-2 border-[#CC1312] bg-background/80 p-4">
+                <h5 className="text-md italic">
+                  Lördag 10 oktober, 12:00
+                </h5>
+                <h3 className="text-xl font-bold uppercase">
+                  Spelkonvent som motstånd och demokratisk infrastruktur
+                </h3>
+                <h4 className="text-lg">Johanna Koljonen</h4>
+              </div>
+            </div>
+
+            <div className="border-l-2 border-[#CC1312] pl-4">
+              <div className="prose text-foreground">
+                <p className="font-bold">
+                  Johanna Koljonen är doktorand i spelforskning på Tampere
+                  University inom EU Horizon-projektet Larpocracy.
+                </p>
+                <p>
+                  I denna halvtidsrapportering av gruppens opublicerade resultat
+                  reder hon ut rollspelens totalt felaktiga historia, diskuterar
+                  nordisk spelkultur som uttryck för nordiska demokratiformer
+                  och samhällsstrukturer, och drar en lans för spelkonventens
+                  och konventsforskningens politiska betydelse.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3">
+            <div className="relative">
+              <Image
+                src={Oceania.src}
+                width={600}
+                height={600}
+                alt="Oceania 2084"
+                className="aspect-square flex-grow-0 object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 border-l-2 border-[#CC1312] bg-background/80 p-4">
+                <h5 className="text-md italic">
+                  Lördag 10 oktober, föredrag 13:00 & spel ca. 14:00
+                </h5>
+                <h3 className="text-xl font-bold uppercase">
+                  Designing Opression: Politics and mechanics in Oceania 2084
+                </h3>
+                <h4 className="text-lg">Johan Eriksson</h4>
+              </div>
+            </div>
+            <div className="border-l-2 border-[#CC1312] pl-4">
+              <div className="prose text-foreground">
+                <p className="font-bold">
+                  Johan Eriksson, the designer of Oceania 2084 will give a talk
+                  titled Designing Oppression: Politics and Mechanics in Oceania
+                  2084. After the talk, you're invited to try the game.
+                </p>
+                <p>
+                  This talk explores how Oceania 2084 uses its mechanics to
+                  define and challenge authoritarianism through procedural
+                  rhetoric. Released in late 2024 after six years of
+                  development, the game quickly won the Italian RPG Magnifico
+                  award and earned praise from reviewers for its bold
+                  approach.{" "}
+                </p>
+                <p>
+                  Oceania 2084 is both a creative experiment and a political
+                  intervention, showing how TTRPGs can grapple with difficult
+                  social and human questions. Earlier academic work, including a
+                  master’s thesis, has examined how the game adapts Orwell’s
+                  1984 into a playable and faithful role-playing experience.
+                </p>
+                <p>
+                  Here, the focus shifts from adaptation to argument: how the
+                  mechanics themselves express particular philosophical and
+                  political stances through their design.
+                </p>
+                <p className="italic">
+                  The talk will be in English or Swedish depending on attendee
+                  preferences.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="italic text-center col-span-5 my-4">Programmet fylls på löpande...</div>
       </article>
     </Suspense>
   );
